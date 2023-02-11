bc36224174ae3dfb67b1694ade74885df3fce968
Olena Voroshnina <yuriyilena67@gmail.com>
2023-02-11 12:04:53 +0100

    added localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,34 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
-import ContactForm from "./ContactForm/ContactForm";
-import ContactList from "./ContactList/ContactList";
-import Filter from "./Filter/Filter";
-import { Container, Title, TitleTwo } from "./App.styled";
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import { Container, Title, TitleTwo } from './App.styled';
 
 export class App extends Component {
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
   }
 
   handleFormSubmit = data => {
@@ -26,54 +40,59 @@ export class App extends Component {
     if (this.state.contacts.find(contact => contact.name === data.name)) {
       alert(`${data.name} is already in contacts.`);
       return;
-    };
-    this.setState((prevState) => ({
+    }
+    this.setState(prevState => ({
       contacts: [newContact, ...prevState.contacts],
     }));
-
   };
-
 
   deleteContact = contactId => {
     this.setState(prevState => ({
-      contacts: prevState.contacts.filter(({id}) => id !== contactId),
-    }))
-  }
+      contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+    }));
+  };
 
-  changeFilter = (e) => {
-    this.setState({filter: e.currentTarget.value})
-  }
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
 
   getVisibleContacts = () => {
     const { filter, contacts } = this.state;
     const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
-  }
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
 
   render() {
-    const { filter, contacts} = this.state;
+    const { filter, contacts } = this.state;
     const visibleContactsList = this.getVisibleContacts();
     return (
       <Container>
         <Title>Phonebook</Title>
-        <ContactForm onSubmit={ this.handleFormSubmit} />
-        
+        <ContactForm onSubmit={this.handleFormSubmit} />
+
         <TitleTwo>Contacts</TitleTwo>
         <Filter value={filter} onChange={this.changeFilter} />
-        
-        {contacts.length > 0 ?
-          <ContactList contacts={visibleContactsList} onDeleteContact={this.deleteContact} /> : null}
-         
+
+        {contacts.length > 0 ? (
+          <ContactList
+            contacts={visibleContactsList}
+            onDeleteContact={this.deleteContact}
+          />
+        ) : null}
       </Container>
     );
   }
-};
+}
 
 App.propTypes = {
-    contacts: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        number: PropTypes.string.isRequired,
-    })),
-    filter: PropTypes.string,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  filter: PropTypes.string,
 };
